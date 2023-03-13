7278e45ab4a45e5746d18bb2000ac7eba2c88153
sophiasadikman-hub <sophia_sadikman@brown.edu>
2023-03-13 14:49:37 -0400

    Create player.m
    main player function

---
+++ player.m
@@ -0,0 +1,16 @@
+function [] = player()
+%call function, display it
+%call function 
+
+while true
+    %function that checks whether word or not
+    word_checking
+
+    %array for points
+
+  
+%initilzie empty list/array and populate with player entries
+%while loop while true (for player to 
+%exits program when timer is out
+
+word = input()
