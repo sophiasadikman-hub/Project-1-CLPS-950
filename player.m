bd681b59e612217b831db51f91db14c06f79cf0a
sophiasadikman-hub <sophia_sadikman@brown.edu>
2023-03-16 13:09:03 -0400

    I just deleted some extra stuff

--- player.m
+++ player.m
@@ -1,14 +1,10 @@
 function [] = player()
-%call function, display it
-%call function 
 
-%all_inputs = 
 points = 0;
 letters = letter_draw();
 disp(letters);
 while true
     word = input('Type word Here: ', "s");
-    %function that checks whether word or not
     checking = word_checking(word);
     if checking ==1
         points = count_points(word, points, letters);
