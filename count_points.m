720de39c4ddb4191825f570e2ddd16868f3edf4c
sophiasadikman-hub <sophia_sadikman@brown.edu>
2023-03-20 16:31:12 -0400

    added comments and deleted old stuff

--- count_points.m
+++ count_points.m
@@ -1,9 +1,9 @@
 function [points, used_vector] = count_points(word, points, random_letters, used_vector); %sets up function to count the points for the inputted words
 %maybe i have to make a for loop checking letter by letter
-if strfind(word, used_vector)
-    disp('You already made this word!')
-    points = points;
-    used_vector = used_vector;
+if strfind(word, used_vector) %if statement to see if the inputted word by the user was already used 
+    disp('You already made this word!') %displays the statement in quotes
+    points = points; %to stop coding errors
+    used_vector = used_vector; %to stop coding errors
     return
 end
 
@@ -12,13 +12,14 @@ for iter = 1:length(word); %loops that goes through each letter in the inputted 
         rl = find(random_letters==word(iter)); %takes the index of where the letter in the user-inputted word is in the random letter array 
         random_letters(rl(1)) = []; %removes that indexed letter from the random_letters vector so that when matlab checks for the next letter of the inputted word it knows that the other letter was already used (every letter from the array of letters provided can only be used once)
     else
-        disp('You did not use the provided letters properly! Try Again!')
+        disp('You did not use the provided letters properly! Try Again!') %displays this statement if letters of the user inputted words are not in the random vector of words the game generated
         return
 
     end
    
 end
-used_vector = [used_vector, word];
+
+used_vector = [used_vector, word]; %updates a vector to add words the user already inputted (used in the player function so that the player cannot repeat words)
 
 points = points + length(word); %adds updated points calculated as amount of letters (i.e. length) of the inputted word
 end
--- player.m
+++ player.m
@@ -2,33 +2,26 @@ function [] = player(); %main player function that calls the other functions to 
 disp('Welcome to Word Cookie!');
 disp('Your job is to make as many words as you can with the given letters!');
 disp('You have three minutes, and your time starts now!')
-%t = timer('TimerFcn','stat=false; disp("Time is Up!")','StartDelay',180); %This is the timer function. The command "TimerFcn" is the command that will be executed when the timer fires. The "StartDelay" property notes how much time is going to be elapsed before the timer fires. The timer counts for 60 seconds, and once the 60 seconds are completed, it uses "disp" to display the message: Time is Up! to the player%
-%start(t) %This begins the timer command. The name of the function is t.
 
-%countdown = 180; %This establishes the 60 seconds as a variable, countdown, just so that the code is cleaner and more intuitive!%  
-%stat = true; %Stat is another variable%
 points = 0; %starts player points off at 0 
-used_words = [];
+used_words = []; %creates an empty vector, used_words, to later populate with words the player has already inputted so that matlab can recognize those words and not count them multiple times in the count_points function
 letters = letter_draw(); %calls the letter_draw function which generates a random set of letters each time and sets that equal to the variable letters
 iniTime = clock;
-limit   = 180;  % Seconds 
-while etime(clock, iniTime) < limit
-%while true
-%while stat == true %a while loop that repeats the word checking and point assigning code (i.e. lets users make words over and over again and updates the point value) until the game is up
+limit   = 180;  % sets time limit to 180 seconds 
+while etime(clock, iniTime) < limit %while loop to keep the game going until the clock/time runs out
     disp(letters); %displays the letters that the letter_draw function generated
     word = input('Type word Here: ', "s"); %code for player to input their word
     checking = word_checking(word); %calls word_checking function to check if the word is in the dictionary, assigns that to variable 'checking'
     if checking ==1 %if 'checking' is true i.e. if matlab finds the inputted word in the dictionary
-       [points, used_words] = count_points(word, points, letters, used_words); %then calls the count_points function to count the points for the word
+       [points, used_words] = count_points(word, points, letters, used_words); %then calls the count_points function to count the points for the word and keep track of the used/inputted words
         pnts = ['Points: ',num2str(points)]; %sets up an updated point value that can be displayed 
         disp(pnts); %actually displays the updated point value to the player
     else 
         disp('Not a word!'); %if matlab does not find the inputted word in the dictionary, then displays 'Not a word' to the player
     end %ends the if statement
 end %ends the while loop
-disp('Thanks for playing! Want to play again?') %Displays a message after the player is done!%
+disp('Time is up! Thanks for playing! Want to play again?') %Displays a message after the player is done!%
 great_job = imread('great_job.png'); %The command "imread" reads an image from a graphics file, in this case, "great_job.png".%
 imshow(great_job) %The command "imshow" diplays the image%
 end   %ends the player function
 
-%delete(t) %It is important to delete the timer objects after they are used%
