0f1e20940d74ee1445a6a3b63bf7f1ea608e41fa
sophiasadikman-hub <sophia_sadikman@brown.edu>
2023-03-20 10:40:49 -0400

    added comments to the main player function and count points function

--- count_points.m
+++ count_points.m
@@ -1,12 +1,10 @@
-function [points] = count_points(word, points, random_letters);
+function [points] = count_points(word, points, random_letters); %sets up function to count the points for the inputted words
 
 
-for iter = 1:length(word);
-    if contains(random_letters, word(iter))
-        done = find(random_letters==word(iter));
-        random_letters(done) = [];
-    else
-        points = points + 0;
+for iter = 1:length(word); %loops that goes through each letter in the inputted word
+    if contains(random_letters, word(iter)) %checks that each letter of the inputted word is contained in the array of random letters that was provided
+        done = find(random_letters==word(iter)); %takes the index of where the letter in the user-inputted word is in the random letter array 
+        random_letters(done) = []; %removes that indexed letter from the random_letters vector so that when matlab checks for the next letter of the inputted word it knows that the other letter was already used (every letter from the array of letters provided can only be used once)
         disp('You did not use the provided letters properly! Try Again!')
         return
 
@@ -15,7 +13,7 @@ end
 
     
 
-points = points + length(word);
+points = points + length(word); %adds updated points calculated as amount of letters (i.e. length) of the inputted word
 end
 
 
--- player.m
+++ player.m
@@ -1,19 +1,19 @@
-function [] = player()
+function [] = player() %main player function that calls the other functions to create one game
 
-points = 0;
-letters = letter_draw();
-disp(letters);
-while true
-    word = input('Type word Here: ', "s");
-    checking = word_checking(word);
-    if checking ==1
-        points = count_points(word, points, letters);
-        pnts = ['Points: ',num2str(points)];
-        disp(pnts);
+points = 0; %starts player points off at 0 
+letters = letter_draw(); %calls the letter_draw function which generates a random set of letters each time and sets that equal to the variable letters
+disp(letters); %displays the letters that the letter_draw function generated
+while true %a while loop that repeats the word checking and point assigning code (i.e. lets users make words over and over again and updates the point value) until the game is up
+    word = input('Type word Here: ', "s"); %code for player to input their word
+    checking = word_checking(word); %calls word_checking function to check if the word is in the dictionary, assigns that to variable 'checking'
+    if checking ==1 %if 'checking' is true i.e. if matlab finds the inputted word in the dictionary
+        points = count_points(word, points, letters); %then calls the count_points function to count the points for the word
+        pnts = ['Points: ',num2str(points)]; %sets up an updated point value that can be displayed 
+        disp(pnts); %actually displays the updated point value to the player
     else 
-        disp('Not a word!');
-    end
+        disp('Not a word!'); %if matlab does not find the inputted word in the dictionary, then displays 'Not a word' to the player
+    end %ends the while loop
 
-end   
-great_job = imread('great_job.png');
-imshow(great_job)
+end   %ends the player function
+great_job = imread('great_job.png'); %loads a great job image
+imshow(great_job) %displays a great job image after the game finishes
